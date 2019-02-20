5eecdb99ff72c8bba695e55a26e9e6c218783344
ideawu <ideawu@me>
2019-02-20 18:40:14 +0800

    update

---
+++ SVGKit/SVGKit.m
@@ -0,0 +1,18 @@
+//
+//  SVGKit.m
+//  SVGKit-iOS
+//
+//  Created by Devon Blandin on 5/13/13.
+//  Copyright (c) 2013 na. All rights reserved.
+//
+
+#import "SVGKit.h"
+
+@implementation SVGKit : NSObject
+
++ (void) enableLogging {
+//    [DDLog addLogger:[DDASLLogger sharedInstance]];
+//    [DDLog addLogger:[DDTTYLogger sharedInstance]];
+}
+
+@end
